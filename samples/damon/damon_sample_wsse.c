201987e3fb0201d10b5ee47defbd3d0988b62269
SeongJae Park <sj@kernel.org>
2023-12-05 02:28:57 +0000

    samples: add working set size estimation DAMON sample module
    Add a sample DAMON application kernel module, namely damon_sample_wsse.
    It is same to the one of the example kernel modules that presented at
    kernel summit 2021 with 10 minutes live coding[1] but updated for the
    latest DAMON interface.
    
    The sample module receives a pid of a process to monitor the access,
    monitors the access to the virtual address space of the process,
    calculate estimated working set size, and repeatedly print it on the
    kernel log.
    
    [1] https://linuxplumbersconf.org/event/11/contributions/984/
    
    Link: https://patch.msgid.link/20231205022858.1540-3-sj@kernel.org
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ samples/damon/damon_sample_wsse.c
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * working set size estimation: monitor access pattern of given process and
+ * print estimated working set size (total size of regions that showing some
+ * access).
+ */
+
+#define pr_fmt(fmt) "damon_sample_wsse: " fmt
+
+#include <linux/damon.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int target_pid __read_mostly;
+module_param(target_pid, int, 0600);
+
+static int damon_sample_wsse_run_store(
+		const char *val, const struct kernel_param *kp);
+
+static const struct kernel_param_ops run_param_ops = {
+	.set = damon_sample_wsse_run_store,
+	.get = param_get_bool,
+};
+
+static bool run __read_mostly;
+module_param_cb(run, &run_param_ops, &run, 0600);
+MODULE_PARM_DESC(run, "Run or stop damon_sample_wsse");
+
+static struct damon_ctx *ctx;
+static struct pid *target_pidp;
+
+static int damon_sample_wsse_after_aggregate(struct damon_ctx *c)
+{
+	struct damon_target *t;
+
+	damon_for_each_target(t, c) {
+		struct damon_region *r;
+		unsigned long wss = 0;
+
+		damon_for_each_region(r, t) {
+			if (r->nr_accesses > 0)
+				wss += r->ar.end - r->ar.start;
+		}
+		pr_info("wss: %lu\n", wss);
+	}
+	return 0;
+}
+
+static int damon_sample_wsse_start(void)
+{
+	struct damon_target *target;
+
+	pr_info("start\n");
+
+	ctx = damon_new_ctx();
+	if (!ctx)
+		return -ENOMEM;
+	if (damon_select_ops(ctx, DAMON_OPS_VADDR)) {
+		damon_destroy_ctx(ctx);
+		return -EINVAL;
+	}
+
+	target = damon_new_target();
+	if (!target) {
+		damon_destroy_ctx(ctx);
+		return -ENOMEM;
+	}
+	damon_add_target(ctx, target);
+	target_pidp = find_get_pid(target_pid);
+	if (!target_pidp) {
+		damon_destroy_ctx(ctx);
+		return -EINVAL;
+	}
+	target->pid = target_pidp;
+
+	ctx->callback.after_aggregation = damon_sample_wsse_after_aggregate;
+	return damon_start(&ctx, 1, true);
+}
+
+static void damon_sample_wsse_stop(void)
+{
+	pr_info("stop\n");
+	if (ctx) {
+		damon_stop(&ctx, 1);
+		damon_destroy_ctx(ctx);
+	}
+	if (target_pidp)
+		put_pid(target_pidp);
+}
+
+static int damon_sample_wsse_run_store(
+		const char *val, const struct kernel_param *kp)
+{
+	bool running = run;
+	int err;
+
+	err = kstrtobool(val, &run);
+	if (err)
+		return err;
+
+	if (run == running)
+		return 0;
+
+	if (run)
+		return damon_sample_wsse_start();
+	damon_sample_wsse_stop();
+	return 0;
+}
+
+static int __init damon_sample_wsse_init(void)
+{
+	return 0;
+}
+
+module_init(damon_sample_wsse_init);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("SeongJae Park");
+MODULE_DESCRIPTION("DAMON sample module for working set size estimation");
