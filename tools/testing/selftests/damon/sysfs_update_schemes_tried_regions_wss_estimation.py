e9d8cf7019e60cb0b81cb07cd350caa4f16bc79f
SeongJae Park <sj@kernel.org>
2026-01-10 09:35:20 -0800

    selftests/damon/wss_estimation: test up to 1000 estimations
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tools/testing/selftests/damon/sysfs_update_schemes_tried_regions_wss_estimation.py
+++ tools/testing/selftests/damon/sysfs_update_schemes_tried_regions_wss_estimation.py
@@ -6,6 +6,24 @@ import time
 
 import _damon_sysfs
 
+def is_test_passed(wss_collected, estimated_wss):
+    wss_collected.sort()
+    percentile = 75
+    nr_collections = len(wss_collected)
+    sample = wss_collected[int(nr_collections * percentile / 100)]
+    error_rate = abs(sample - estimated_wss) / estimated_wss
+    print('number of collections: %d' % nr_collections)
+    print('%d-th percentile (%d) error %f' %
+            (percentile, sample, error_rate))
+
+    acceptable_error_rate = 0.2
+    if error_rate < acceptable_error_rate:
+        return True
+
+    print('the error rate is not acceptable (> %f)' %
+            acceptable_error_rate)
+    return False
+
 def main():
     # access two 10 MiB memory regions, 2 second per each
     sz_region = 10 * 1024 * 1024
@@ -27,8 +45,9 @@ def main():
         print('kdamond start failed: %s' % err)
         exit(1)
 
+    test_passed = False
     wss_collected = []
-    while proc.poll() is None and len(wss_collected) < 40:
+    while proc.poll() is None:
         time.sleep(0.1)
         err = kdamonds.kdamonds[0].update_schemes_tried_bytes()
         if err != None:
@@ -37,21 +56,22 @@ def main():
 
         wss_collected.append(
                 kdamonds.kdamonds[0].contexts[0].schemes[0].tried_bytes)
+
+        nr_collections = len(wss_collected)
+        if nr_collections > 0 and nr_collections % 40 == 0:
+            test_passed = is_test_passed(wss_collected, sz_region)
+            if test_passed:
+                break
+            if nr_collections >= 1000:
+                break
     proc.terminate()
 
-    wss_collected.sort()
-    percentile = 75
-    sample = wss_collected[int(len(wss_collected) * percentile / 100)]
-    error_rate = abs(sample - sz_region) / sz_region
-    print('%d-th percentile (%d) error %f' %
-            (percentile, sample, error_rate))
-    acceptable_error_rate = 0.2
-    if error_rate > acceptable_error_rate:
-        print('the error rate is not acceptable (> %f)' %
-                acceptable_error_rate)
-        print('samples are as below')
-        print('\n'.join(['%d' % wss for wss in wss_collected]))
-        exit(1)
+    if test_passed is True:
+        return
+
+    print('samples are as below')
+    print('\n'.join(['%d' % wss for wss in wss_collected]))
+    exit(1)
 
 if __name__ == '__main__':
     main()
