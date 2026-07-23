076906d19134ec8c1a39c3b4daba0ad81419e7aa
SJ Park <sj@kernel.org>
2026-07-23 07:43:35 -0700

    mm/damon/paddr: respect folio boundary for DAMOS_[DE]ACTIVATE
    Sashiko.
    
    [1] https://lore.kernel.org/20260517234112.89245-1-sj@kernel.org
    
    Signed-off-by: SJ Park <sj@kernel.org>

--- mm/damon/paddr.c
+++ mm/damon/paddr.c
@@ -395,11 +395,16 @@ static inline unsigned long damon_pa_de_activate(
 
 	addr = damon_pa_phys_addr(r->ar.start, addr_unit);
 	while (addr < damon_pa_phys_addr(r->ar.end, addr_unit)) {
+		phys_addr_t folio_addr;
+
 		folio = damon_get_folio(PHYS_PFN(addr));
 		if (damon_pa_invalid_damos_folio(folio, s)) {
 			addr += PAGE_SIZE;
 			continue;
 		}
+		folio_addr = PFN_PHYS(folio_pfn(folio));
+		if (folio_addr < addr)
+			goto put_folio;
 
 		if (damos_pa_filter_out(s, folio))
 			goto put_folio;
@@ -412,7 +417,7 @@ static inline unsigned long damon_pa_de_activate(
 			folio_deactivate(folio);
 		applied += folio_nr_pages(folio);
 put_folio:
-		addr += folio_size(folio);
+		addr = folio_addr + folio_size(folio);
 		folio_put(folio);
 	}
 	s->last_applied = folio;
