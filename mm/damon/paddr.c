24ea9f71be7bfea3609014bd2d1f7443441d7b1f
SJ Park <sj@kernel.org>
2026-07-23 07:41:01 -0700

    mm/damon/paddr: respect folio boundary for DAMOS_PAGEOUT
    Sashiko finding.
    
    [1] https://lore.kernel.org/20260517234112.89245-1-sj@kernel.org
    
    Signed-off-by: SJ Park <sj@kernel.org>

--- mm/damon/paddr.c
+++ mm/damon/paddr.c
@@ -349,11 +349,16 @@ static unsigned long damon_pa_pageout(struct damon_region *r,
 
 	addr = damon_pa_phys_addr(r->ar.start, addr_unit);
 	while (addr < damon_pa_phys_addr(r->ar.end, addr_unit)) {
+		phys_addr_t folio_addr;
+
 		folio = damon_get_folio(PHYS_PFN(addr));
 		if (damon_pa_invalid_damos_folio(folio, s)) {
 			addr += PAGE_SIZE;
 			continue;
 		}
+		folio_addr = PFN_PHYS(folio_pfn(folio));
+		if (folio_addr < addr)
+			goto put_folio;
 
 		if (damos_pa_filter_out(s, folio))
 			goto put_folio;
@@ -369,7 +374,7 @@ static unsigned long damon_pa_pageout(struct damon_region *r,
 		else
 			list_add(&folio->lru, &folio_list);
 put_folio:
-		addr += folio_size(folio);
+		addr = folio_addr + folio_size(folio);
 		folio_put(folio);
 	}
 	if (install_young_filter)
