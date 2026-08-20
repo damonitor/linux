f72997223632fceaa361a3a86eabb34dec6b6ec0
Andrew Morton <akpm@linux-foundation.org>
2026-08-20 09:15:38 -0700

    mm-provide-vma_is_cow_mapping-and-remove-is_cow_mapping-fix
    fix kerneldoc comment typo, per Lorenzo
    
    Link: https://lore.kernel.org/aob1goSSPH6sTN9y@gremlin
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Pedro Falcato <pfalcato@suse.de>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2318,7 +2318,7 @@ static inline bool vma_flags_is_cow_mapping(const vma_flags_t *flags)
 
 /**
  * vma_is_cow_mapping() - Is this VMA a CoW mapping?
- * @desc: The VMA to check.
+ * @vma: The VMA to check.
  *
  * See vma_flags_is_cow_mapping() for details.
  *
